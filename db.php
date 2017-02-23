4b7b8fe543a6d11abe065487a2fc149ba9736e04
dabnich <dabnich@gmail.com>
2017-02-24 00:24:18 +0100

    0.1.2

--- db.php
+++ db.php
@@ -1,19 +1,30 @@
 <?php 
 /*
-if($_POST["save"]==1){
-	if(saveResult()) echo "result saving succesful";
+if(isset($_POST)){
+	if($_POST["save"]==1){
+		if(saveResult()) echo "result saving succesful";
+		else echo "Error: result not saving";
+	}
 	else echo "Error: result not saving";
 }
-else echo "Error: result not saving";
+*/
+if(isset($_POST)){
+	if($_POST["save"]==1)
+		saveResult();
+}
 
 function saveResult(){
 	if($handle = fopen("results.csv", "a+")){
-		$text = $_POST["game"]+"|"+date("d-m-Y H:i:s")+"|"+$_POST["time"]+"|"+$_POST["actions"]+chr(10);
+		$text = $_POST["game"]."|".date("d-m-Y H:i:s")."|".$_POST["time"]."|".$_POST["actions"].chr(10);
+		//$text = $_POST["game"]."|".date("d-m-Y H:i:s")."|".$_POST["time"]"|".$_POST["actions"];
+		//echo $_POST["game"];
+		echo $text;
+		//echo $_POST["actions"];
 		if(fwrite($handle, $text)) return true;
 		return false;
 	}
 	return false;
 }
-*/
-print_r($_POST);
+
+//if(isset($_POST)) print_r($_POST);
 ?>
